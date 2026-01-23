6ef9c049ab523def24150ffba0145ce2064308b7
Athish-07 <athishharis1207@gmail.com>
2026-01-23 17:27:25 +0530

    small edit

--- microservices-eccom/service-registry/src/main/java/com/example/service_registry/ServiceRegistryApplication.java
+++ microservices-eccom/service-registry/src/main/java/com/example/service_registry/ServiceRegistryApplication.java
@@ -10,7 +10,8 @@ public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceRegistryApplication.class, args);
-        System.out.println("Checking entry Author : Athish test");
+        System.out.println("Checking entry Author ");
+        System.out.println("second checking");
 	}
 
 }
